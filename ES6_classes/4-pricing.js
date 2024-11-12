b7f2ff5dd77338120e8cf74e0dcd5a3a7cfbf786
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-11-12 13:15:26 +0100

    4-pricing.js Pricing class created

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,38 @@
+import Currency from './3-currency.js';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+
+  get amount() {
+    return this._amount;
+  }
+
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+
+  get currency() {
+    return this._currency;
+  }
+
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
