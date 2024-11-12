575ee8e068d829146f074c81762946371c701b26
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-11-12 13:58:35 +0100

    fix indentation

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -2,27 +2,27 @@ import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
     constructor(sqft, floors) {
-        super(sqft);
-        this._floors = floors;
+      super(sqft);
+      this._floors = floors;
     }
 
     get floors() {
-        return this._floors;
+      return this._floors;
     }
 
     set floors(floors) {
-        this._floors = floors;
+      this._floors = floors;
     }
 
     get sqft() {
-        return this._sqft;
+      return this._sqft;
     }
 
     set sqft(sqft) {
-        this._sqft = sqft;
+      this._sqft = sqft;
     }
 
     evacuationWarningMessage() {
-        return `Evacuate slowly the ${this._floors} floors`;
+      return `Evacuate slowly the ${this._floors} floors`;
     }
 }
