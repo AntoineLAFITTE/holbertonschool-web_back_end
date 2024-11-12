db8e7dfbd421e44402b5e04ac5364820dd279554
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-11-12 13:39:28 +0100

    revert

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,6 +1,6 @@
 import Currency from './3-currency.js';
 
-export default class Pricing extends Currency {
+export default class Pricing {
   constructor(amount, currency) {
     this.amount = amount;
     this.currency = currency;
