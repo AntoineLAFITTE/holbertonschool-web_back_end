0bd711222ddf86ed2ec911ec26be5efa93ea44f4
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-11-20 14:48:09 +0100

    6-final-user.js try to fix

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,6 +1,5 @@
-/* eslint-disable */
-import signUpUser from "./4-user-promise.js";
-import uploadPhoto from "./5-photo-reject.js";
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
     const signUpPromise = signUpUser(firstName, lastName);
@@ -8,10 +7,10 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
 
     return Promise.allSettled([signUpPromise, uploadPromise]).then((results) => {
         return results.map((result) => {
-            if (result.status === 'fullfilled') {
-                return { status: 'fullfilled', value: result.value };
+            if (result.status === 'fulfilled') {
+                return { status: 'fulfilled', value: result.value };
             }
-            return { status: 'rejected', value: result.reason};
+            return { status: 'rejected', value: result.reason };
         });
     });
 }
