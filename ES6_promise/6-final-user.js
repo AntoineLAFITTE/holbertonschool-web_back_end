6a5f5ce3f75b04401c040d081036503a2622124a
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-11-20 14:53:05 +0100

    6-final-user.js explicitly returning into array to try for chekcker

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,13 +5,11 @@ import uploadPhoto from './5-photo-reject.js';
 export default function handleProfileSignup(firstName, lastName, fileName) {
     const signUpPromise = signUpUser(firstName, lastName);
     const uploadPromise = uploadPhoto(fileName);
-
-    return Promise.allSettled([signUpPromise, uploadPromise]).then((results) => {
-        return results.map((result) => {
-            if (result.status === 'fulfilled') {
-                return { status: 'fulfilled', value: result.value };
-            }
-            return { status: 'rejected', value: result.reason };
-        });
-    });
+    return Promise.allSettled([signUpPromise, uploadPromise])
+        .then((results) =>
+            results.map((result) => ({
+                status: result.status,
+                value: result.status === 'fulfilled' ? result.value : result.reason,
+            }))
+        );
 }
