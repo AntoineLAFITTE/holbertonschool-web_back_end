c62df1495f92639cf130adf7be7d1c683b057470
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-12-02 13:38:44 +0100

    2-floor.py done

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Type-annotated function that returns the floor of the float"""
+
+
+def floor(n: float) -> int:
+    """
+    Takes a float n and returns the floor of the float as an integer.
+
+    Parameters:
+    n: a float wich its floor value is to be returned.
+
+    Returns:
+    int: The largest integer less than or equal to n.
+    """
+    return int(n)
