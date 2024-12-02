22e71f1bf2b5f84c76dea46fea28ddeb293abaa0
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-12-02 13:29:22 +0100

    1-concat.py

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""concat function with type anotation"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Takes 2 strings and return their conctenated result.
+
+    Parameters:
+    srt1: first string to concatenate
+    str2: second string to concatenate
+
+    Return:
+    Concatenated string str1 and str2
+    """
+    return str1 + str2
