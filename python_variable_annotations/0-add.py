c2530872d345e5c95c00463824f707b560785ed0
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-12-02 13:10:28 +0100

    0-add.py documentation zzzz

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
-
-
 def add(a: float, b: float) -> float:
-    """Takes 2 floats and return their sum as a float"""
+    """
+    Takes 2 floats and return their sum as a float
+
+    Parameters:
+    a (float): first number to add.
+    b (float): second number to add
+
+    Returns:
+    floats: The sum of 'a" and 'b'.
+    """
+
     return a + b
