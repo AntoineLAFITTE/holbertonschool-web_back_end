acdb5d1d74f849329c9775e93c8a7240ced2e759
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-12-09 15:43:33 +0100

    0-async_generator.py done

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import random
+import asyncio
+
+async def async_generator():
+    """Yields random numbers from 0 to 10 waiting 1 sec each number"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
