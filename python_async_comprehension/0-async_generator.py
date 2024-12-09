fc96cd7287f8becca7d3a946a5069af65e28591d
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-12-09 16:00:28 +0100

    0-async_generator.py typing added

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
-
 import random
 import asyncio
+from typing import AsyncGenerator
 
 
-async def async_generator():
+"""Async Generator function"""
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
     """Yields random numbers from 0 to 10 waiting 1 sec each number"""
     for _ in range(10):
         await asyncio.sleep(1)
